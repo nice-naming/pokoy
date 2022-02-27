68117be6539c8457bf72ee5c7ccf80f70f45cff9
Martyn <zogacc@gmail.com>
2022-02-27 16:29:16 +0200

    fix: fix auth bug and change switcher action state

--- src/features/home/components/views-switcher/views-switcher.styles.ts
+++ src/features/home/components/views-switcher/views-switcher.styles.ts
@@ -6,18 +6,26 @@ export const SwipeButton = styled.button`
   align-items: center;
   position: absolute;
   bottom: 0;
+  left: 50%;
   background-color: transparent;
-  color: var(--c-foreground);
+  color: var(--c-extra-gray);
   transition: none;
   padding: 1rem 2rem;
   margin: 1rem;
+  transform: translateX(-50%);
   margin-bottom: 7rem;
   border-radius: 3rem;
+  transition: transform ease-out 0.1s;
+
+  &:active {
+    transform: translateX(-50%) scale(0.9);
+  }
 `
 
 interface Props {
   isActive: boolean
 }
+
 export const Circle = styled.span<Props>`
   aspect-ratio: 1;
   width: 1.5rem;
--- src/features/user-stats/user-stats.tsx
+++ src/features/user-stats/user-stats.tsx
@@ -2,7 +2,10 @@ import { useCallback, useEffect, useState } from "react"
 import { User } from "@firebase/auth"
 import { UserSerie } from "react-charts"
 import { PokoyChartData, UserStatsData } from "shared/types"
-import { fetchAndsetChartData, getStats } from "./get-data"
+import {
+  fetchAndsetChartData as fetchAndSetChartData,
+  getStats,
+} from "./get-data"
 import { StatsChart } from "./components/stats-chart/stats-chart.component"
 import { StatsNumbers } from "./components/stats-numbers/stats-numbers.component"
 import { Wrapper } from "./user-stats.styles"
@@ -15,10 +18,11 @@ export const UserStats: React.FC<Props> = ({ user }) => {
   const [chartData, setChartData] = useState<UserSerie<PokoyChartData>[]>([])
   const [statsData, setStatsData] = useState<UserStatsData | null>(null)
 
-  const memoizedGetChartData = useCallback(fetchAndsetChartData, [])
+  const memoizedGetChartData = useCallback(fetchAndSetChartData, [])
   const memoizedGetStats = useCallback(getStats, [])
 
   useEffect(() => {
+    if (!user) return
     memoizedGetChartData(setChartData, user)
     memoizedGetStats(setStatsData, user)
     // eslint-disable-next-line react-hooks/exhaustive-deps
