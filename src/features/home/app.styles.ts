09e922b7f9b08fa031bc84b405b60c783a061851
Martyn <zogacc@gmail.com>
2022-02-27 16:28:12 +0200

    chore(refactoring): extract reusable styled component

--- src/features/home/app.styles.ts
+++ src/features/home/app.styles.ts
@@ -1,25 +1,4 @@
-import { TABLET_MIN_WIDTH } from "shared/constants"
 import styled from "styled-components/macro"
-
-export const Wrapper = styled.div`
-  display: flex;
-  text-align: center;
-  justify-content: center;
-  position: relative;
-  width: 100%;
-  height: 100%;
-
-  @media screen and (max-width: ${TABLET_MIN_WIDTH}) {
-    & {
-      padding: 0 4rem;
-    }
-  }
-`
-
-export const SwipeableView = styled.div`
-  height: 100%;
-  overflow: hidden;
-`
 
 export const ProgressSpiralWrapper = styled.div`
   display: flex;
