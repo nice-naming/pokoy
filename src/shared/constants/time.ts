49ff2885cba441b332f8fd43d3901aebfc5bdbe5
Martyn <zogacc@gmail.com>
2022-04-27 14:53:08 +0400

    chore: refactor time constants

--- src/shared/constants/time.ts
+++ src/shared/constants/time.ts
@@ -1,2 +1,8 @@
 export const SECS_IN_MIN = 60
-export const MAX_TIMER_SECONDS = 1260 // NOTE: equal to 21 minutes
+// NOTE: equal to 21 minutes
+export const MAX_TIMER_SECONDS = SECS_IN_MIN * 21
+export const MINS_IN_HOUR = 60
+export const SECS_IN_HOUR = SECS_IN_MIN * MINS_IN_HOUR
+export const SECS_IN_DAY = SECS_IN_HOUR * 24
+export const MILLIS_IN_DAY = SECS_IN_DAY * 1000
+export const INITIAL_MEDITATION_DURATION = 0
