218934b1bbcffcaadcf2329217670728569112e3
Martyn <zogacc@gmail.com>
2022-05-29 21:28:30 +0400

    perf: rewrite spiral animation on css modules

--- src/features/home/components/fib-spiral/fib-spiral.component.tsx
+++ src/features/home/components/fib-spiral/fib-spiral.component.tsx
@@ -7,14 +7,11 @@ import {
   PATH_TO_DRAWN,
   START_SPIRAL_OFFSET,
   MAX_SPIRAL_VALUE,
+  INIT_STROKE_DASHARRAY,
 } from "./fib-spiral.constants"
-import {
-  StyledSpiralBackground,
-  StyledSpiralPath,
-  StyledSvg,
-} from "./fib-spiral.styles"
 import { getTimerProgress } from "./get-timer-progress"
 import { getFloorFibonacciDiscrete } from "shared/utils/getFloorFibonacciDiscrete"
+import styles from "./fib-spiral.module.css"
 
 // TODO: move to constants
 const ALMOST_DONE_PERCENT = 0.995
@@ -36,35 +33,44 @@ export const FibSpiral: React.FC<Props> = ({ seconds, authLoading }) => {
     return getColorFromCSSVar(colorCSSVarName)
   }, [minutes])
 
+  const isStarted = progress > START_SPIRAL_OFFSET
+  const isStartedClassName = isStarted ? styles.spiralPathStarted : null
+  const isEmptyClassName = isEmpty ? styles.spiralPathEmpty : null
+  const spiralForegroundClassNames = `${styles.spiralPath} ${isStartedClassName} ${isEmptyClassName}`
+
+  const isLoadingClassName = authLoading ? styles.spiralBackgroundLoading : null
+  const spiralBackgroundClassNames = `${styles.spiralBackground} ${isLoadingClassName} spiral-trial`
+
+  const isSpinningClassName = isStarted ? styles.svgSpinning : null
+  const fibSpiralClassNames = `${styles.svg} ${isSpinningClassName}`
+
   return (
-    <StyledSvg
-      progress={progress}
+    <svg
+      className={fibSpiralClassNames}
       width="100%"
       height="100%"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 760 769"
     >
-      <StyledSpiralBackground
-        authLoading={authLoading}
-        className="spiral-trial"
+      <path
+        className={spiralBackgroundClassNames}
+        d={PATH_TO_DRAWN}
         strokeLinecap="round"
         strokeLinejoin="round"
         fillOpacity="0"
-        d={PATH_TO_DRAWN}
       />
 
-      <StyledSpiralPath
-        color={fibColor}
-        offset={START_SPIRAL_OFFSET + progress}
-        isEmpty={isEmpty}
-        strokeDashoffset={START_SPIRAL_OFFSET}
-        stroke="currentColor"
+      <path
+        className={spiralForegroundClassNames}
+        stroke={fibColor}
+        strokeDasharray={INIT_STROKE_DASHARRAY}
+        strokeDashoffset={START_SPIRAL_OFFSET + progress}
+        d={PATH_TO_DRAWN}
         strokeLinecap="round"
         strokeLinejoin="round"
         fillOpacity="0"
-        d={PATH_TO_DRAWN}
       />
-    </StyledSvg>
+    </svg>
   )
 }
