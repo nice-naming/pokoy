89a319bb41125682fefc87f4940c7ee2d04150a9
Martyn <zogacc@gmail.com>
2022-06-07 10:10:53 +0400

    feat: extract react router as a component and add routing

--- src/app-router.tsx
+++ src/app-router.tsx
@@ -1,22 +1,47 @@
 import { App } from "features/home"
 import { DevPage } from "features/dev-page"
 import { SignInPage } from "features/sign-in-page/sign-in-page"
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 import { RequireAuth } from "shared/components/require-auth"
+import { AboutPage } from "features/about-page/about-page"
+import { Tutorial } from "features/about-page/components/tutorial/tutorial.component"
+import { HowItWorks } from "features/about-page/components/how-it-works/how-it-works.component"
+import { About } from "features/about-page/components/about.component"
 
-export const AppRouter = () => (
-  <Routes>
-    <Route path="/login" element={<SignInPage />} />
-    <Route
-      path="/"
-      element={
-        <RequireAuth>
-          <App />
-        </RequireAuth>
-      }
-    />
-    <Route path="dev" element={<DevPage />} />
-    {/* // TODO: add route for authentication */}
-    {/* <Route path="sign-in" element={<App />} /> */}
-  </Routes>
-)
+const NoMatch = () => {
+  return (
+    <>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </>
+  )
+}
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<SignInPage />} />
+
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <App />
+          </RequireAuth>
+        }
+      />
+
+      <Route path="/about" element={<AboutPage />}>
+        <Route path="" element={<About />}></Route>
+        <Route path="tutorial" element={<Tutorial />}></Route>
+        <Route path="how-it-works" element={<HowItWorks />}></Route>
+      </Route>
+
+      <Route path="/dev" element={<DevPage />} />
+
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+  )
+}
