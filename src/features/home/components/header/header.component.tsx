5f2fc046645be27b436321f0988b7c2b246968c6
Martyn <zogacc@gmail.com>
2022-06-15 11:59:25 +0400

    chore: improve header

--- src/features/home/components/header/header.component.tsx
+++ src/features/home/components/header/header.component.tsx
@@ -1,18 +1,25 @@
 import { useOnline } from "@saulpalv/useonline"
+import { useLocation } from "react-router-dom"
 import { AppUpdater } from "../app-updater"
 import { OfflineStatus } from "../offline-status"
 import { SignOut } from "../sign-out"
-import { Wrapper } from "./header.styles"
+import { StyledLink, Wrapper } from "./header.styles"
 
 interface Props {}
 
 export const Header: React.FC<Props> = () => {
   const isOnline = useOnline()
+  const currentPath = useLocation().pathname
+  const labelTo = currentPath === "/" ? "About" : "Timer"
+  const pathTo = currentPath === "/" ? "/about" : "/"
 
   return (
     <Wrapper>
       <SignOut />
+      <StyledLink to={pathTo}>{labelTo}</StyledLink>
+
       {!isOnline && <OfflineStatus />}
+
       <AppUpdater />
     </Wrapper>
   )
