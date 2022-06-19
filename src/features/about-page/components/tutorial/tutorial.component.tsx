eebaf09faaa956cb77e16a820d62b31156e5ea54
Martyn <zogacc@gmail.com>
2022-06-19 19:39:45 +0400

    fix: remove annoying sound in tutorial

--- src/features/about-page/components/tutorial/tutorial.component.tsx
+++ src/features/about-page/components/tutorial/tutorial.component.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react"
+import { useCallback } from "react"
 import { virtualize } from "react-swipeable-views-utils"
 import { AboutPageWrapper } from "../../about-page.styles"
 import SwipeableViews from "react-swipeable-views"
@@ -6,9 +6,6 @@ import { ViewsSwitcher } from "features/home/components/views-switcher/views-swi
 import { tutorialSlideRenderer } from "./tutorial-slide-renderer"
 import { useAppDispatch, useAppSelector } from "store"
 import { aboutPageActions, aboutPageSelectors } from "../../about-page.slice"
-import useSound from "use-sound"
-import bellSfx from "shared/assets/sounds/meditation-bell-sound.mp3"
-import clickSfx from "shared/assets/sounds/finger-snap.mp3"
 
 const SLIDES_COUNT = 5
 const VirtualizedSwipeableViews = virtualize(SwipeableViews)
@@ -16,24 +13,11 @@ const VirtualizedSwipeableViews = virtualize(SwipeableViews)
 export const Tutorial = () => {
   const dispatch = useAppDispatch()
   const slideIndex = useAppSelector(aboutPageSelectors.getTutorialSlideIndex)
-  const [playClick] = useSound(clickSfx)
-  const [playBell] = useSound(bellSfx, {
-    volume: 0.3,
-  })
 
   const setSlideIndex = useCallback(() => {
     const newIndex = slideIndex < SLIDES_COUNT - 1 ? slideIndex + 1 : 0
     dispatch(aboutPageActions.setTutorialSlideIndex(newIndex))
   }, [dispatch, slideIndex])
-
-  useEffect(() => {
-    if (slideIndex === 0) {
-      playClick()
-    }
-    if (slideIndex === 3) {
-      playBell()
-    }
-  }, [slideIndex, playClick, playBell])
 
   return (
     <AboutPageWrapper>
