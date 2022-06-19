66c8680ea1d8d41de04000b67c4ce1a589bda922
Martyn <zogacc@gmail.com>
2022-06-19 19:40:29 +0400

    fix: change horizontal align for header items

--- src/features/home/components/header/header.styles.ts
+++ src/features/home/components/header/header.styles.ts
@@ -4,19 +4,15 @@ import styled from "styled-components/macro"
 
 export const Wrapper = styled.header`
   width: 100%;
-  display: grid;
-  grid-template-columns: auto min-content min-content min-content;
+  display: flex;
   white-space: nowrap;
   column-gap: 3rem;
   align-items: baseline;
+  justify-content: end;
   color: var(--c-gray);
   font-size: 1rem;
   padding: 1rem;
   z-index: 1;
-
-  > *:first-child {
-    margin-right: auto;
-  }
 
   @media screen and (min-width: ${TABLET_MIN_WIDTH}) {
     padding: 2rem 1rem;
