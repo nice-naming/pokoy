8ef26ee81446560bbd2b313493190d0883fe6192
Martyn <zogacc@gmail.com>
2022-09-13 20:00:02 +0400

    fix: change global constants for meditation stages

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -17,7 +17,22 @@ export enum CSS_COLOR_VARIABLES {
   CYAN = "--c-cyan",
   BLUE = "--c-blue",
   MAGENTA = "--c-magenta",
-  FOREGROUND = "--c-foreground",
+  FOREGROUND = "--c-foreground"
+}
+
+export enum CSS_COLOR_VALUES {
+  BACKGROUND = "#2b2b2b",
+  DARKEN_GRAY = "#333",
+  DARK_GRAY = "#656565",
+  GRAY = "#7f7f7f",
+  RED = "#ff8686",
+  ORANGE = "#ff8b16",
+  YELLOW = "#afaf00",
+  GREEN = "#00d800",
+  CYAN = "#00dcdc",
+  BLUE = "#abd5ff",
+  MAGENTA = "#ffcee6",
+  FOREGROUND = "#f5f5f5"
 }
 
 export const LOCAL_CACHE_FIELD_NAME = "pokoy-local-sessions"
@@ -26,5 +41,5 @@ export const REQUEST_STATUS_TO_COLOR_MAP = new Map<RequestStatus, string>([
   [RequestStatus.NONE, "var(--c-gray)"],
   [RequestStatus.REQUEST, "var(--c-yellow)"],
   [RequestStatus.SUCCESS, "var(--c-green)"],
-  [RequestStatus.FAILURE, "var(--c-red)"],
+  [RequestStatus.FAILURE, "var(--c-red)"]
 ])
